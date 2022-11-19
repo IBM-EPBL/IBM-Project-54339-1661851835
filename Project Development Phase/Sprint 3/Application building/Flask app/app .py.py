83b3a704c10cee49acad40c0805466f6d9814d87
Sathish02022002 <113543704+Sathish02022002@users.noreply.github.com>
2022-11-19 21:23:30 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 3/Application building/Flask app/app .py.py
@@ -0,0 +1,53 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import joblib
+import random
+
+app=Flask(__name__,template_folder="templates")
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    try:
+        if request.method == 'POST':
+            l=[]
+            l.append(float(request.form['id']))
+            l.append(float(request.form['cycle']))
+            l.append(float(request.form['set1']))
+            l.append(float(request.form['set2']))
+            l.append(float(request.form['set3']))
+            l.append(float(request.form['s1']))
+            l.append(float(request.form['s2']))
+            l.append(float(request.form['s3']))
+            l.append(float(request.form['s4']))
+            l.append(float(request.form['s5']))
+            l.append(float(request.form['s6']))
+            l.append(float(request.form['s7']))
+            l.append(float(request.form['s8']))
+            l.append(float(request.form['s9']))
+            l.append(float(request.form['s10']))
+            l.append(float(request.form['s11']))
+            l.append(float(request.form['s12']))
+            l.append(float(request.form['s13']))
+            l.append(float(request.form['s14']))
+            l.append(float(request.form['s15']))
+            l.append(float(request.form['s16']))
+            l.append(float(request.form['s17']))
+            l.append(float(request.form['s18']))
+            l.append(float(request.form['s19']))
+            l.append(float(request.form['s20']))
+            l.append(float(request.form['s21']))
+            print(l)
+            if predict(l):
+                return render_template('result.html',data="problem")
+            else:
+                return render_template('result.html',data="normal")
+    except:
+        return render_template('result.html',data="error")
+
+
+if __name__ == '__main__':
+      app.run(debug=False)
